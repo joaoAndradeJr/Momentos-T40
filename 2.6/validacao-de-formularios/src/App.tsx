bed24f537224de61f22562cdf42f27e75c20b8ff
Joao Andrade Jr <fullservicemanaus@gmail.com>
2024-04-05 09:08:46 -0300

    codigo ao final do momento

---
+++ 2.6/validacao-de-formularios/src/App.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+
+function App() {
+  const [email, setEmail] = useState('');
+  const [showForm, setShowForm] = useState(true);
+  const [isBtnDisabled, setIsBtnDisabled] = useState(true);
+
+  const validateEmail = (value) => {
+    const validRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}/;
+    const isEmailValid = validRegex.test(value);
+    return !isEmailValid;
+  }
+
+  const handleChange = ({ target }) => {
+   setEmail(target.value)
+   const isValidEmail = validateEmail(target.value);
+   setIsBtnDisabled(isValidEmail);
+  };
+
+  return (
+    <>
+    {
+      showForm ? (
+        <form>
+          <label htmlFor="email-input">
+            Email
+            <input
+              type="email"
+              value={ email }
+              onChange={ handleChange }
+            />
+          </label>
+          <button
+            type="button"
+            onClick={ () => setShowForm(false) }
+            disabled={ isBtnDisabled }
+          >
+            Login
+          </button>
+        </form>
+      )
+      : (
+        <section>
+          <h1>Bem-vindo(a)</h1>
+        </section>
+      )
+    }
+    </>
+  );
+}
+
+export default App
