5486abb265f45ec59e483f5241f69dcc38228a33
Joao Andrade Jr <fullservicemanaus@gmail.com>
2024-04-24 18:09:07 -0300

    refactoring: 3.2

--- 3.2/props-children-outlet/src/pages/Home.tsx
+++ 3.2/props-children-outlet/src/pages/Home.tsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/user')
+    .then((response) => response.json())
+    .then((data) => setUsers(data.users))
+    .catch((error) => console.error(error.message));
+  }, []);
+
   return (
-    <div>Home</div>
+    <main>
+      <div>
+        {
+          users.map((user) => (
+            <section key={ user.id }>
+              <img src={ user.image } alt={ `${user.firstName} photo` } />
+              <p>{ `${user.firstName} ${user.lastName}` }</p>
+            </section>
+          ))
+        }
+      </div>
+    </main>
   );
 }
